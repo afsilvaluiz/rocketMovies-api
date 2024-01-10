7cf1efa062b1d21d8a4c929a13a76483f1bdd72d
luiz <afsilvaluiz@gmail.com>
2024-01-10 15:04:33 -0300

    JSON response method configured

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params
+app.post("/users", (request, response) => {
+  const { name, email, password} = request.body
 
-  response.send(`
-  Message ID: ${id}.
-  For the user: ${user}.
-  `)
+  response.json({ name, email, password })
 })
 
 const PORT = 3333
