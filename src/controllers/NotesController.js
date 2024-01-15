e3933f2857e06862c377a12f304c3088d627d703
luiz <afsilvaluiz@gmail.com>
2024-01-15 09:51:10 -0300

    Method 'Register Note' implemented.

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,30 @@
+const knex = require("../database/knex")
+
+class NotesController {
+  async create(request, response) {
+    const { title, description, rating, tags } = request.body
+    const { user_id } = request.params
+
+    const [movie_notes_id] = await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return{
+        movie_notes_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("movie_tags").insert(tagsInsert)
+
+    return response.json()
+  }
+}
+
+module.exports = NotesController
+
