1eca6ec0e11d9d31dad3611204cb8f797f4ca521
luiz <afsilvaluiz@gmail.com>
2024-01-28 16:30:52 -0300

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,14 @@ const { Router } = require('express');
 const usersRouter = require('./users.routes');
 const notesRouter = require('./notes.routes')
 const tagsRouter = require('./tags.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router();
+
 routes.use('/users', usersRouter);
+routes.use("/sessions", sessionsRouter)
 routes.use("/movie_notes", notesRouter)
 routes.use("/movie_tags", tagsRouter)
+
 
 module.exports = routes;
