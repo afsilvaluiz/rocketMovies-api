3edd82f670fcd4cef1d8123c8a8cb8f3fb67be20
luiz <afsilvaluiz@gmail.com>
2024-02-09 11:55:13 -0300

    feat: dotenv config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 require('express-async-errors');
+require('dotenv/config')
+
+const uploadConfig = require('./configs/upload')
 const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
-const uploadConfig = require('./configs/upload')
 
 const cors = require('cors')
 const express = require('express');
@@ -33,5 +35,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
