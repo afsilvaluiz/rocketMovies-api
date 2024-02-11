e3d5469486c314979350d4494b220221ce2de933
luiz <afsilvaluiz@gmail.com>
2024-02-11 15:12:56 -0300

    test: use describe to group tests

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,16 +1,19 @@
 const UserCreateService = require('./UserCreateService');
-const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
 
-it('user should be created', async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  }
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+describe('UserCreateService', () => {
+  it('user should be created', async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    }
 
-  expect(userCreated).toHaveProperty("id");
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
+
+    expect(userCreated).toHaveProperty("id");
+  })
 })
